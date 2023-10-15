5c477984ecc24ccdb47e9b7a40e50898b342bfb5
MariiaSam <kontragent@ukr.net>
2023-10-15 16:58:45 +0300

    change project

---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model = [
+    { type: 'title', value: 'Hello world from js???' },
+  
+    { type: 'text', value: 'here we go some text' },
+  
+    { type: 'columns', value: ['111111111', '22222222', '333333333'] },
+    {type: 'image', value: './assents/image.png'}
+  ];
+  
